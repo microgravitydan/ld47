47949d9be2e7f33b31aa350871d014e1e916af10
Dan Manley <dan@dcmanley.com>
2020-10-02 19:43:45 -0500

    Theme: Stuck in a Loop

---
+++ Assets/Scripts/ShipRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipRotation : MonoBehaviour {
+    // Variables
+    [SerializeField]
+    public float RotationPeriod; //Rotation Period (8seconds/rotation for mars)
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Rotate
+
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ld47
 
-Ludum Dare 47 Compo
+Ludum Dare 47 Compo - Stuck in a Loop
+
+## Outline
+    * ArtificialGravity
+        * 0.38 Gravity on floor
+        * 4.72m/s
+        * ≈8 seconds per interval
