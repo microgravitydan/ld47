2666e5389f191c7431b80c001c792b899b0a8820
Dan Manley <dan@dcmanley.com>
2020-10-03 17:13:44 -0500

    Rotation Sorta Works

--- Assets/Scripts/ShipRotation.cs
+++ Assets/Scripts/ShipRotation.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ShipRotation : MonoBehaviour {
     // Variables
+    private float xAngle, yAngle, zAngle;
+    
     [SerializeField]
     public float rotationPeriod; //Rotation Period (8seconds/rotation for mars)
     [SerializeField]
     private float rotationAngle;
+    [SerializeField]
     private GameObject rotatingObject;
 
     // Start is called before the first frame update
@@ -18,7 +21,7 @@ public class ShipRotation : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         //Rotate
-        rotationAngle = Time.deltaTime - rotationPeriod;
-        rotatingObject.transform.localRotation = rotationAngle;
+        rotationAngle = Time.time / rotationPeriod;
+        rotatingObject.transform.Rotate(1/rotationPeriod, yAngle, zAngle, Space.World);
     }
 }
