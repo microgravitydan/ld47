97bef81664b1c5c40fe01d100d6f2c7d9fa45e22
Dan Manley <dan@dcmanley.com>
2020-10-03 21:48:41 -0500

    Beginning Control Scripts

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    private Vector3 playerInput = Vector3.zero;
+    
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        playerInput = Vector3.zero;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.z = Input.GetAxis("Vertical");
+    }
+}
--- Assets/Scripts/ShipRotation.cs
+++ Assets/Scripts/ShipRotation.cs
@@ -3,25 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipRotation : MonoBehaviour {
-    // Variables
-    private float xAngle, yAngle, zAngle;
-    
     [SerializeField]
     public float rotationPeriod; //Rotation Period (8seconds/rotation for mars)
-    [SerializeField]
-    private float rotationAngle;
-    [SerializeField]
-    private GameObject rotatingObject;
 
     // Start is called before the first frame update
     void Awake() {
-        
     }
 
     // Update is called once per frame
     void Update() {
         //Rotate
-        rotationAngle = Time.time / rotationPeriod;
-        rotatingObject.transform.Rotate(1/rotationPeriod, yAngle, zAngle, Space.World);
+        transform.Rotate(4/rotationPeriod, 0, 0, Space.Self);
     }
 }
